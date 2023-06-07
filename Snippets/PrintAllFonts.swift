efe3238b0a078874ea2e04d6525a59b5f1fa43b5
Arseniy Zolotarev <aguznenkovv@yandex.ru>
2023-06-07 03:07:10 +0200

    Add printing all font func

---
+++ Snippets/PrintAllFonts.swift
@@ -0,0 +1,11 @@
+//
+//  CoreDataManager.swift
+//  Swift Snippets
+//
+//  github.com/032nnxkitty
+//
+
+UIFont.familyNames.forEach { familyName in
+    let fontNames = UIFont.fontNames(forFamilyName: familyName)
+    print(familyName, fontNames)
+}
